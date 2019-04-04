ed3e9edc6104d1adaea6a765602584ceaed42a31
Emile Nsengimana <emilereas7@gmail.com>
2019-04-04 22:23:51 +0200

    allow admin to view specifi user account,(de)activate or delete user account

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -1,3 +1,7 @@
+const viewBankAccount = document.getElementById('modal-search');
+const btnSearch = document.getElementById('btn-search');
+const viewUserAccount = document.getElementById('modal-view-account');
+
 // ================================= MODAL USERS ==============================
 const btnUsers = document.getElementById('btn-users');
 btnUsers.addEventListener('click', () => {
@@ -21,3 +25,40 @@ const btnLogout = document.getElementById('btn-admin-logout');
 btnLogout.addEventListener('click', () => {
     location.href = 'index.html';
 });
+// ================================= ACTIVATE || DEACTIVATE CLIENT ==============================
+const btnChange = document.getElementById('btn-status');
+btnChange.addEventListener('click', () => {
+    console.log(btnChange.className);
+    if(btnChange.className === 'btn btn-warn'){
+        document.getElementById('txt-status').value = 'Dormant'
+        btnChange.innerHTML = 'Activate';
+        btnChange.classList.add('btn-success');
+    }else{
+        document.getElementById('txt-status').value = 'Active';
+        btnChange.innerHTML = 'Deactivate';
+        btnChange.classList.remove('btn-success');
+    }
+    
+});
+// ---------------------------------- CLOSE MODAL -------------------------------------------------
+const btnClose = document.getElementById('close-search');
+btnClose.addEventListener('click', hideModal);
+function hideModal() {
+    document.getElementById('modal-search').style.display = 'none';
+}
+
+// ---------------------------------- SEARCH -------------------------------------------------
+function view(){
+    viewBankAccount.style.display = 'block';
+}
+btnSearch.addEventListener('click', view);
+// ================================= USER ACCOUNT ==============================
+function accounts(){
+    viewUserAccount.style.display = 'block';
+}
+const hideUserAccount = document.getElementById('close-account');
+hideUserAccount.addEventListener('click', hideAccounts);
+function hideAccounts() {
+    document.getElementById('modal-view-account').style.display = 'none';
+}
+document.getElementById('btn-search-user').addEventListener('click', accounts);
