6c657d494b73a62c6f8a918d645654a234ca53a1
Emile Nsengimana <emilereas7@gmail.com>
2019-04-08 22:13:11 +0200

    [ch-#165197316] rewrite css and js location reference

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const viewBankAccount = document.getElementById('modal-search');
 const btnSearch = document.getElementById('btn-search');
 const viewUserAccount = document.getElementById('modal-view-account');
@@ -5,65 +6,65 @@ const viewUserAccount = document.getElementById('modal-view-account');
 // ================================= MODAL USERS ==============================
 const btnUsers = document.getElementById('btn-users');
 btnUsers.addEventListener('click', () => {
-    document.getElementById('display').style.display = 'none';
-    document.getElementById('modal-users').style.display = 'block';
-    btnUsers.classList.add('active');
-    btnBankAcct.classList.remove('active');
+  document.getElementById('display').style.display = 'none';
+  document.getElementById('modal-users').style.display = 'block';
+  btnUsers.classList.add('active');
+  btnBankAcct.classList.remove('active');
 });
 
 // ================================= BANK ACCOUNTS ==============================
 const btnBankAcct = document.getElementById('btn-bank-acct');
 btnBankAcct.addEventListener('click', () => {
-    document.getElementById('modal-users').style.display = 'none';
-    document.getElementById('display').style.display = 'block';
-    btnBankAcct.classList.add('active');
-    btnUsers.classList.remove('active');
+  document.getElementById('modal-users').style.display = 'none';
+  document.getElementById('display').style.display = 'block';
+  btnBankAcct.classList.add('active');
+  btnUsers.classList.remove('active');
 });
 
 // ================================= SIGN OUT ==============================
 const btnLogout = document.getElementById('btn-admin-logout');
 btnLogout.addEventListener('click', () => {
-    location.href = 'index.html';
+  location.href = 'UI/index.html';
 });
 // ================================= ACTIVATE || DEACTIVATE CLIENT ==============================
 const btnChange = document.getElementById('btn-status');
 btnChange.addEventListener('click', () => {
-    if(btnChange.className === 'btn btn-warn'){
-        document.getElementById('txt-status').value = 'Dormant'
-        document.getElementById('txt-status-2').value = 'Dormant'
-        btnChange.innerHTML = 'Activate';
-        btnChange.classList.add('btn-success');
-    }else{
-        document.getElementById('txt-status').value = 'Active';
-        document.getElementById('txt-status-2').value = 'Dormant'
+  if (btnChange.className === 'btn btn-warn') {
+    document.getElementById('txt-status').value = 'Dormant';
+    document.getElementById('txt-status-2').value = 'Dormant';
+    btnChange.innerHTML = 'Activate';
+    btnChange.classList.add('btn-success');
+  } else {
+    document.getElementById('txt-status').value = 'Active';
+    document.getElementById('txt-status-2').value = 'Dormant';
 
-        btnChange.innerHTML = 'Deactivate';
-        btnChange.classList.remove('btn-success');
-    }
-    
+    btnChange.innerHTML = 'Deactivate';
+    btnChange.classList.remove('btn-success');
+  }
 });
 // ---------------------------------- CLOSE MODAL -------------------------------------------------
+function hideModal() {
+  document.getElementById('modal-search').style.display = 'none';
+}
 const btnClose = document.getElementById('close-search');
 btnClose.addEventListener('click', hideModal);
-function hideModal() {
-    document.getElementById('modal-search').style.display = 'none';
-}
 
 // ---------------------------------- SEARCH -------------------------------------------------
-function view(){
-    viewBankAccount.style.display = 'block';
+function view() {
+  viewBankAccount.style.display = 'block';
 }
 btnSearch.addEventListener('click', view);
 
 // ================================= USER ACCOUNT ==============================
-function accounts(){
-    viewUserAccount.style.display = 'block';
+function accounts() {
+  viewUserAccount.style.display = 'block';
 }
 const hideUserAccount = document.getElementById('close-account');
-hideUserAccount.addEventListener('click', hideAccounts);
 function hideAccounts() {
-    document.getElementById('modal-view-account').style.display = 'none';
+  document.getElementById('modal-view-account').style.display = 'none';
 }
+hideUserAccount.addEventListener('click', hideAccounts);
+
 document.getElementById('btn-search-user').addEventListener('click', accounts);
 
 // ---------------------------------- ADD USER -------------------------------------------------
@@ -72,8 +73,8 @@ const btnAddUser = document.getElementById('btn-create-user');
 
 const modalCreateUserAcct = document.getElementById('modal-add-user');
 btnCancelAddUser.addEventListener('click', () => {
-    modalCreateUserAcct.style.display = 'none';
+  modalCreateUserAcct.style.display = 'none';
 });
 btnAddUser.addEventListener('click', () => {
-    modalCreateUserAcct.style.display = 'block';
+  modalCreateUserAcct.style.display = 'block';
 });
--- UI/js/client.js
+++ UI/js/client.js
@@ -1,15 +1,17 @@
+/* eslint-disable no-undef */
 const btnCreateAcct = document.getElementById('btn-create-account');
 const btnSaveAcct = document.getElementById('btn-add-acct');
 const btnCloseCreateAcct = document.getElementById('close');
+const btnTransactionHistory = document.getElementById('btn-transaction-history');
 
 btnCreateAcct.addEventListener('click', () => {
-    document.getElementById('modal-account').style.display = 'block';
-    document.getElementById('all').style.opacity = 0.3;
+  document.getElementById('modal-account').style.display = 'block';
+  document.getElementById('all').style.opacity = 0.3;
 });
 let i = 2;
 btnSaveAcct.addEventListener('click', () => {
-    i = i+1;
-    document.getElementById('acctInfo').innerHTML  += `
+  i += 1;
+  document.getElementById('acctInfo').innerHTML += `
     <h3>Account ${i}</h3>
                 <div class="form-group">
                     <label for="">Account type:</label>
@@ -26,52 +28,51 @@ btnSaveAcct.addEventListener('click', () => {
     `;
 });
 btnCloseCreateAcct.addEventListener('click', () => {
-    document.getElementById('modal-account').style.display = 'none';
-    document.getElementById('all').style.opacity = 1;
+  document.getElementById('modal-account').style.display = 'none';
+  document.getElementById('all').style.opacity = 1;
 });
-// ========================================== CLIENT LOG-OUT ============================================
+// ========================================== CLIENT LOG-OUT =======================================
 const btnClientLogout = document.getElementById('btn-client-logout');
 btnClientLogout.addEventListener('click', () => {
-    location.href = 'index.html';
+  location.href = 'UI/index.html';
 });
 
 // ========================================== TRANSFER ============================================
 const btnTransfer = document.getElementById('btn-menu-transfer');
-const btnMyAccount = document.getElementById('btn-menu-myaccount')
+const btnMyAccount = document.getElementById('btn-menu-myaccount');
 btnTransfer.addEventListener('click', () => {
-    document.getElementById('acctInfo').style.display = 'none';
-    document.getElementById('btn-create-account').style.display = 'none';
-    document.getElementById('transfer').style.display = 'block';
-    document.getElementById('transaction-history').style.display = 'none';
+  document.getElementById('acctInfo').style.display = 'none';
+  document.getElementById('btn-create-account').style.display = 'none';
+  document.getElementById('transfer').style.display = 'block';
+  document.getElementById('transaction-history').style.display = 'none';
 
-    if(btnTransfer.className === 'btn-default'){
-        btnMyAccount.classList.remove('active');
-        btnTransfer.classList.add('active');
-        btnTransactionHistory.classList.remove('active');
-    }
+  if (btnTransfer.className === 'btn-default') {
+    btnMyAccount.classList.remove('active');
+    btnTransfer.classList.add('active');
+    btnTransactionHistory.classList.remove('active');
+  }
 });
-btnMyAccount.addEventListener('click', () =>{
-    document.getElementById('acctInfo').style.display = 'block';
-    document.getElementById('btn-create-account').style.display = 'block';
-    document.getElementById('transfer').style.display = 'none';
-    document.getElementById('transaction-history').style.display = 'none';
+btnMyAccount.addEventListener('click', () => {
+  document.getElementById('acctInfo').style.display = 'block';
+  document.getElementById('btn-create-account').style.display = 'block';
+  document.getElementById('transfer').style.display = 'none';
+  document.getElementById('transaction-history').style.display = 'none';
 
-    if(btnMyAccount.className === 'btn-default'){
-        btnMyAccount.classList.add('active');
-        btnTransfer.classList.remove('active');
-        btnTransactionHistory.classList.remove('active');
-    }
+  if (btnMyAccount.className === 'btn-default') {
+    btnMyAccount.classList.add('active');
+    btnTransfer.classList.remove('active');
+    btnTransactionHistory.classList.remove('active');
+  }
 });
 // ========================================== TRANSACTION HISTORY ==========================
-const btnTransactionHistory = document.getElementById('btn-transaction-history');
-btnTransactionHistory.addEventListener('click', () =>{
-    document.getElementById('acctInfo').style.display = 'none';
-    document.getElementById('transfer').style.display = 'none';
-    document.getElementById('btn-create-account').style.display = 'none';
-    document.getElementById('transaction-history').style.display = 'block';
-    if(btnTransactionHistory.className === 'btn-default'){
-        btnMyAccount.classList.remove('active');
-        btnTransfer.classList.remove('active');
-        btnTransactionHistory.classList.add('active');
-    }
+btnTransactionHistory.addEventListener('click', () => {
+  document.getElementById('acctInfo').style.display = 'none';
+  document.getElementById('transfer').style.display = 'none';
+  document.getElementById('btn-create-account').style.display = 'none';
+  document.getElementById('transaction-history').style.display = 'block';
+  if (btnTransactionHistory.className === 'btn-default') {
+    btnMyAccount.classList.remove('active');
+    btnTransfer.classList.remove('active');
+    btnTransactionHistory.classList.add('active');
+  }
 });
