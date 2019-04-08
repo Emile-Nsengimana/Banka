0fd22e89340f3eb7e8c3395537bf64fac54e5793
Emile Nsengimana <emilereas7@gmail.com>
2019-04-08 18:47:37 +0200

    [ft-#165168351] allow registered user to login

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,5 +4,6 @@ import userControl from '../controllers/userController';
 const route = express.Router();
 
 route.post('/api/v1/auth/signup', userControl.signup);
+route.post('/api/v1/auth/login', userControl.login);
 
 export default route;
