b76dc5c5acdfb9b3616b0b0a3dac382734ac9bed
Emile Nsengimana <emilereas7@gmail.com>
2019-04-10 14:20:07 +0200

    resolving conflict

--- src/routes/router.js
+++ src/routes/router.js
@@ -10,10 +10,7 @@ route.post('/api/v1/auth/signup', userControl.signup);
 route.post('/api/v1/auth/login', userControl.login);
 route.post('/api/v1/accounts', auth, accountControl.createAccount);
 route.patch('/api/v1/account/:id', auth, accountControl.changeAccountStatus);
-<<<<<<< HEAD
 route.delete('/api/v1/account/:id', auth, accountControl.deleteAccount);
-=======
 route.post('/api/v1/transactions/:accountNo/debit', auth, transactionControl.debitAccount);
->>>>>>> challenge-ii
 
 export default route;
