212d116f0a0ae155342b8b7a5221d84e185636bf
Emile Nsengimana <emilereas7@gmail.com>
2019-04-16 17:32:05 +0200

    ch(signup-endpoint) validate if a password is strong enough

--- src/controllers/validation/userSchema.js
+++ src/controllers/validation/userSchema.js
@@ -1,11 +1,20 @@
 import joi from 'joi';
+import PasswordComplexity from 'joi-password-complexity';
 
+const complexityOptions = {
+  min: 10,
+  max: 30,
+  lowerCase: 1,
+  upperCase: 1,
+  numeric: 1,
+  requirementCount: 2,
+};
 const userSchema = joi.object().keys({
   id: joi.number().required(),
   firstName: joi.string().alphanum().min(3).required(),
   lastName: joi.string().alphanum().min(3).required(),
   email: joi.string().email().required(),
-  password: joi.string().min(8).required(),
+  password: new PasswordComplexity(complexityOptions),
   type: joi.string().min(4).required(),
   isAdmin: joi.boolean().required(),
 });
